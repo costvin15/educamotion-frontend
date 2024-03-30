928b5d2ec0811bff0d679909f8c3b295aee62598
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2024-03-30 01:23:49 -0300

    feat: Mensagem de erro corrigida

--- src/app/dashboard/components/DocumentsGrid.tsx
+++ src/app/dashboard/components/DocumentsGrid.tsx
@@ -96,9 +96,9 @@ export default function DocumentsGrid() {
                 initial={{ x: 10, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: 10, opacity: 0 }}
-                transition={{ duration: 1 }}
+                transition={{ duration: 0.2 }}
               >
-                <p>Something went wrong. Please try again later.</p>
+                <p>Algo deu errado ao carregar as apresentações.</p>
               </motion.div>
             </Box>
           )}
