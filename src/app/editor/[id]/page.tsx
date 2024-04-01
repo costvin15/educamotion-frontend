87ca553ad43693e118a32da1b9638381e451852e
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2024-04-01 00:39:36 -0300

    feat: Skeletons adicionados para representação de loading

--- src/app/editor/[id]/page.tsx
+++ src/app/editor/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
-import { Box, Card, CardMedia, Grid, IconButton, Paper, Snackbar, Toolbar, Typography } from '@mui/material';
+import { Box, Card, CardMedia, Grid, IconButton, Paper, Skeleton, Snackbar, Toolbar, Typography } from '@mui/material';
 
 import AddIcon from '@mui/icons-material/Add';
 import SlideshowIcon from '@mui/icons-material/Slideshow';
@@ -43,29 +43,37 @@ export default function Edit({ params }: { params: { id: string } }) {
   const [presentation, setPresentation] = useState({} as Presentation);
   const [slides, setSlides] = useState([] as Slide[]);
   const [savedSuccessfully, setSavedSuccessfully] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     if (params.id) {
       (async () => {
+        console.log('Fetching presentation')
         const presentation = await getPresentationDetails(params.id);
         setPresentation(presentation);
       })();
     }
-  }, [params.id]);
+  }, []);
 
   useEffect(() => {
     if (presentation.totalSlides > 0) {
       (async () => {
+        var progress = 0;
         const images = await Promise.all(presentation.slides.map(async (slide) => {
-          return await getThumbnail(presentation.presentationId, slide.objectId);
+          const result = await getThumbnail(presentation.presentationId, slide.objectId);
+          progress += 1;
+          setProgress(progress / presentation.totalSlides * 100);
+          return result;
         }));
         setSlides(images);
+        setLoading(false);
       })();
     }
-  }, [presentation.slides]);
+  }, [presentation]);
 
   const savePresentation = async (presentation: Presentation, slides: Slide[]) => {
-    const response = await client.put(`/presentation/update/${presentation.presentationId}`, {
+    await client.put(`/presentation/update/${presentation.presentationId}`, {
       slides: slides.map((slide) => slide.objectId),
     });
     setSavedSuccessfully(true);
@@ -80,9 +88,13 @@ export default function Edit({ params }: { params: { id: string } }) {
         onClose={() => setSavedSuccessfully(false)}
         anchorOrigin={{ vertical: 'top', horizontal: 'right' }} />
       <Toolbar>
-        <Typography>
-          {presentation.title}
-        </Typography>
+        {loading && (
+          <Skeleton variant='text' className='w-96' />
+        )}
+
+        {!loading && (
+          <Typography>{presentation.title}</Typography>
+        )}
         <Box className='flex-grow' />
         <Box>
           <IconButton size='large'>
@@ -101,39 +113,60 @@ export default function Edit({ params }: { params: { id: string } }) {
       <Box className='flex-1 overflow-hidden'>
         <Grid container>
           <Grid item md={10}>
-            <ImageGallery
-              infinite
-              showPlayButton={false}
-              showBullets
-              showThumbnails={false}
-              showFullscreenButton={false}
-              items={slides}
-            />
+            {loading && (
+              <Skeleton
+                className='h-full mx-5'
+                variant='rectangular' />
+            )}
+
+            {!loading && (
+              <ImageGallery
+                infinite
+                showPlayButton={false}
+                showBullets
+                showThumbnails={false}
+                showFullscreenButton={false}
+                items={slides}
+              />
+            )}
           </Grid>
-          <Grid item md={2} className='h-[calc(100vh-70px)] overflow-y-auto'>
-            <Paper className='pr-2 mb-2 h-full'>
-              <Reorder.Group
-                axis='y'
-                layoutScroll
-                values={slides}
-                onReorder={setSlides}
-                className='list-none p-0'
-              >
-                {slides.map((item) => (
-                  <Reorder.Item className='mb-2' key={item.original} value={item}>
-                    <Card>
-                      <CardMedia
-                        referrerPolicy='no-referrer'
-                        draggable={false}
-                        component='img'
-                        image={item.original}
-                      />
-                    </Card>
-                  </Reorder.Item>
+
+          {loading && (
+            <Grid item md={2} className='h-[calc(100vh-70px)]'>
+              <Paper className='pr-2 mb-2 h-full'>
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <Skeleton key={index} variant='rounded' className='h-36 mb-2' />
                 ))}
-              </Reorder.Group>
-            </Paper>
-          </Grid>
+              </Paper>
+            </Grid>
+          )}
+
+          {!loading && (
+            <Grid item md={2} className='h-[calc(100vh-70px)] overflow-y-auto'>
+              <Paper className='pr-2 mb-2 h-full'>
+                <Reorder.Group
+                  axis='y'
+                  layoutScroll
+                  values={slides}
+                  onReorder={setSlides}
+                  className='list-none p-0'
+                >
+                  {slides.map((item) => (
+                    <Reorder.Item className='mb-2' key={item.original} value={item}>
+                      <Card>
+                        <CardMedia
+                          referrerPolicy='no-referrer'
+                          draggable={false}
+                          component='img'
+                          image={item.original}
+                        />
+                      </Card>
+                    </Reorder.Item>
+                  ))}
+                </Reorder.Group>
+              </Paper>
+            </Grid>
+          )}
         </Grid>
       </Box>
     </Box>
