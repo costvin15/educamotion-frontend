0a2996368dbceac325999e5e68871d715b93916f
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2024-04-07 05:35:45 -0300

    feat: Buscando dados de uma enquete

--- src/app/activity/poll/index.tsx
+++ src/app/activity/poll/index.tsx
@@ -1,3 +1,6 @@
+import client from '@/client';
+import { useEffect, useState } from 'react';
+
 type Activity = {
   presentationId: string;
   activityId: string;
@@ -5,14 +8,43 @@ type Activity = {
   objectId: string;
 };
 
-export default function PollActivity(
-  {activity}: {activity: Activity}
-) {
+type Choice = {
+  id: string;
+  pollId: string;
+  description: string;
+};
+
+type Poll = {
+  id: string;
+  presentationId: string;
+  question: string;
+  choices: Choice[];
+};
+
+async function getPollActivity(activityId: string) : Promise<Poll> {
+  const response = await client.get(`/poll/${activityId}`);
+  return response.data;
+}
+
+export default function PollActivity({activity}: {activity: Activity}) {
+  const [poll, setPoll] = useState<Poll>();
+
+  useEffect(() => {
+    (async () => {
+      const poll = await getPollActivity(activity.activityId);
+      setPoll(poll);
+    })();
+  }, []);
+
   return (
     <div className='h-[calc(100vh-70px)]'>
       <h1>Activity Poll</h1>
       <p>Activity ID: {activity.activityId}</p>
+      <p>Activity Name: {poll?.question}</p>
       <p>Presentation ID: {activity.presentationId}</p>
+      {poll?.choices.map(choice => (
+        <p>{choice.description}</p>
+      ))}
     </div>
   );
 }
