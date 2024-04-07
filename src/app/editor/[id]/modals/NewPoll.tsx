2415150168ced3f13ae3ed6ee3b1aa39971afb48
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2024-04-07 00:46:49 -0300

    feat: Inserindo novas opções conforme interação do usuário

--- src/app/editor/[id]/modals/NewPoll.tsx
+++ src/app/editor/[id]/modals/NewPoll.tsx
@@ -1,5 +1,7 @@
-import { Backdrop, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, styled } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Backdrop, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, styled } from '@mui/material';
+
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const BackdropForModal = styled(Backdrop)(({theme}) => ({
   zIndex: theme.zIndex.modal,
@@ -10,8 +12,15 @@ type Presentation = {
 };
 
 export default function NewPoll({presentation, open, onClose} : {presentation: Presentation, open: boolean, onClose: () => void}) {
+  const [currentLastOptionId, setCurrentLastOptionId] = useState(1);
+  const [options, setOptions] = useState([
+    {
+      id: 1,
+      value: ''
+    }
+  ]);
+
   useEffect(() => {
-    console.log(presentation);
   }, []);
 
   return (
@@ -23,23 +32,55 @@ export default function NewPoll({presentation, open, onClose} : {presentation: P
           <DialogContentText>
             Qual será o título da enquete?
           </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin='dense'
-            label='Título da enquete'
-            fullWidth
-            variant='standard' />
+          <FormControl className='mt-3' variant='outlined' fullWidth>
+            <InputLabel>Título da enquete</InputLabel>
+            <OutlinedInput
+              type='text'
+              label='Título da enquete'
+              fullWidth />
+          </FormControl>
           <DialogContentText className='pt-3'>
             Quais serão as opções da enquete?
           </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin='dense'
-            label='Opção 1'
-            fullWidth
-            variant='standard' />
+          {options.map((option, index) => (
+            <FormControl key={index} className='mt-3' variant='outlined' fullWidth>
+              <InputLabel>Opção {index + 1}</InputLabel>
+              <OutlinedInput
+                fullWidth
+                type='text'
+                label='Opção 1'
+                value={option.value}
+                onChange={(e) => {
+                  const newOptions = options.map((o) => {
+                    if (o.id === option.id) {
+                      return {...o, value: e.target.value};
+                    }
+                    return o;
+                  });
+
+                  if (option.id === currentLastOptionId) {
+                    setOptions([...newOptions, {id: currentLastOptionId + 1, value: ''}]);
+                    setCurrentLastOptionId(currentLastOptionId + 1);
+                  } else {
+                    setOptions(newOptions);
+                  }
+                }}
+                endAdornment={
+                  option.id !== currentLastOptionId && <>
+                    <InputAdornment position='end'>
+                      <IconButton
+                        edge='end'
+                        onClick={() => {
+                          setOptions(options.filter((o) => o.id !== option.id));
+                        }}
+                      >
+                        <RemoveIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  </>
+                } />
+            </FormControl>
+          ))}
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose}>Cancelar</Button>
